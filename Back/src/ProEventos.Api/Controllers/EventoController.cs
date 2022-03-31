4e8e305607976f5421e6f8c571c2e875ed95f85b
alissonbeloti <alisson.beloti@gmail.com>
2022-03-31 17:34:56 -0300

    inicial

---
+++ Back/src/ProEventos.Api/Controllers/EventoController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> eventos = new Evento[] {
+                new Evento
+                {
+                    EventoId = 1,
+                    Tema = "Angular 11 e .NET 5",
+                    Local = "Belo Horizonte",
+                    Lote = "1º Lote",
+                    QtdPessoas = 250,
+                    DataEvento = DateTime.Now.AddDays(2).ToString(),
+                    ImagemUrl = "foto.png"
+                },
+                new Evento
+                {
+                    EventoId = 2,
+                    Tema = "Angular e sua novidades",
+                    Local = "São Paulo",
+                    Lote = "2º Lote",
+                    QtdPessoas = 350,
+                    DataEvento = DateTime.Now.AddDays(3).ToString(),
+                    ImagemUrl = "foto1.png"
+                }
+            };
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return this.eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento Get(int id)
+        {
+            return this.eventos.FirstOrDefault(x => x.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Post(int id)
+        {
+            return $"exemplo de put: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"exemplo de de HttpDelete: {id}";
+        }
+    }
+}
+
