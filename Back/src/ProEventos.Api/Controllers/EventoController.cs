c8026b52655511cb02e827d284a922810d766976
alissonbeloti <alisson.beloti@gmail.com>
2022-04-01 09:35:37 -0300

    migrations

--- Back/src/ProEventos.Api/Controllers/EventoController.cs
+++ Back/src/ProEventos.Api/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.Api.Data;
 using ProEventos.Api.Models;
 
 namespace ProEventos.Api.Controllers
@@ -13,43 +14,23 @@ namespace ProEventos.Api.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> eventos = new Evento[] {
-                new Evento
-                {
-                    EventoId = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Belo Horizonte",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemUrl = "foto.png"
-                },
-                new Evento
-                {
-                    EventoId = 2,
-                    Tema = "Angular e sua novidades",
-                    Local = "São Paulo",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString(),
-                    ImagemUrl = "foto1.png"
-                }
-            };
-        public EventoController()
-        {
+        private readonly DataContext context;
 
+        public EventoController(DataContext context)
+        {
+            this.context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return this.eventos;
+            return this.context.Eventos;
         }
 
         [HttpGet("{id}")]
         public Evento Get(int id)
         {
-            return this.eventos.FirstOrDefault(x => x.EventoId == id);
+            return this.context.Eventos.FirstOrDefault(x => x.EventoId == id);
         }
 
         [HttpPost]
---
+++ Back/src/ProEventos.Api/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Api.Models;
+
+namespace ProEventos.Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Evento> Eventos { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt) { }
+    }
+}
